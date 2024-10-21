943c8f252d41c571f1623c6b401f50df929fcafc
Develper-ke <antonytorotich@gmail.com>
2024-10-21 15:11:52 +0300

    New host

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,25 +1,28 @@
 <footer id="footer" class="footer">
-    <div class="container ">
+    <div class="container">
         <div class="row">
-            <div class="col pull-left text-white" style="font-size: 14px; color:rgb(255, 255, 255);">copyright&copy;
-                @php echo date('Y');@endphp.{{ env('APP_NAME') }}.
+            <div class="col-auto pull-left text-white" style="font-size: 14px; color:rgb(255, 255, 255);">
+                <span class="align-bottom">
+                    copyright&copy; @php echo date('Y');@endphp.{{ env('APP_NAME') }}.
+                </span>
             </div>
             <!-- /.social links -->
-            <div class="col social text-center">
+            <div class="col-auto social text-center">
                 <ul>
-                    <li><a class=" " href="https://twitter.com/founddocument"><i class="fa fa-twitter"></i></a></li>
-                    <li><a class=" " href="https://www.facebook.com/founddocument" data-wow-delay="0.2s"><i
+                    <li><a class="" href="https://twitter.com/founddocument"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="" href="https://www.facebook.com/founddocument" data-wow-delay="0.2s"><i
                                 class="fa fa-facebook"></i></a></li>
-                    <li><a class=" " href="https://plus.google.com/" data-wow-delay="0.4s"><i
+                    <li><a class="" href="https://plus.google.com/" data-wow-delay="0.4s"><i
                                 class="fa fa-google-plus"></i></a></li>
-                    <li><a class=" " href="https://instagram.com/founddocument" data-wow-delay="0.6s"><i
+                    <li><a class="" href="https://instagram.com/founddocument" data-wow-delay="0.6s"><i
                                 class="fa fa-instagram"></i></a></li>
 
                 </ul>
             </div>
-            <div class="col pull-right pt-8"><span class="pull-right"
-                    style="font-size: 14px; color:rgb(255, 255, 255);"> Powered by <a
-                        href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a></span>
+            <div class="col-auto pull-right">
+                <span class="pull-right text-white" style="font-size: 14px; color:rgb(255, 255, 255);">
+                    Powered by <a href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a>
+                </span>
             </div>
             <a href="#" class="scrollToTop"><i class="pe-7s-up-arrow pe-va"></i></a>
         </div>
